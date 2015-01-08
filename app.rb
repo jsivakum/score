88d073ed419b29254d94bb339e1e2a273a89cb67
Jeyananthan Sivakumaran & Sylwia M Bialczak <student@epicodus.com>
2015-01-08 11:18:59 -0800

    Working Scrabble Score.

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 require('sinatra')
 require('sinatra/reloader')
-require('/lib/score.rb')
+require('./lib/score.rb')
 
 get('/') do
   erb(:index)
 end
 
 get('/results') do
-  @user_input = params.fetch('word').score()
+  @user_input = params.fetch('word')
+  @points = @user_input.score()
   erb(:results)
 end
